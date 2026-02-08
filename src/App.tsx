c1ace2417744a32558a86d2b8633581735a23e4f
macooo3 <marcoysu765@gmail.com>
2026-02-08 16:13:53 -0500

    created elements of hompage

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Will you be my Valentines?</h1>
+      <button className='btn yes-btn'>Yes</button>  
+      <button className='btn no-btn'>No</button>
+    </>
+  )
+}
+
+export default App
